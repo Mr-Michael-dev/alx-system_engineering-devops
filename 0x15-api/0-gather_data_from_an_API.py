cb7416aad9d1d06365416607328bf4f2cc2f7ea9
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-04-23 03:56:09 +0100

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+for a given employee ID, returns information about his/her TODO list progress
+using REST API
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2 or not sys.argv[1].isdigit():
+        print(f"Usage: {sys.argv[0]} <employee ID>")
+        exit()
+
+    user_id = int(sys.argv[1])
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    EMPLOYEE = ""
+    TOTAL_TASKS = 0
+    DONE = 0
+
+    for user in users:
+        if user.get('id') == user_id:
+            EMPLOYEE = user.get('username')
+
+    for todo in todos:
+        if todo.get('userId') == user_id:
+            TOTAL_TASKS += 1
+            if todo.get('completed') == True:
+                DONE += 1
+
+    print(f"Employee {EMPLOYEE} is done with tasks ({DONE}/{TOTAL_TASKS}):")
+
+    for task in todos:
+        if task.get('userId') == user_id:
+            print(f"\t{task['title']}")
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+Introductoo to API
