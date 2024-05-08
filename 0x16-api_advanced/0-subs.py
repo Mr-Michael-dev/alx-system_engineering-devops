4a4cbe9633bc41369027fc4d4a1fba4e852fea5b
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-05-08 05:10:38 +0100

    Top Ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about".format(subreddit)
-    
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
     headers = {'User-Agent': 'ubuntu:apiProject/v1'}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+ function that queries the Reddit API and returns
+ the title of top 10 hot posts
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=9"
+
+    headers = {'User-Agent': 'ubuntu:apiProject/v1'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        hot_posts_titles = []
+        for post in posts:
+            post_data = post['data']
+            hot_posts_titles.append(post_data['title'])
+        for title in hot_posts_titles:
+            print(f"{title}")
+    else:
+        print('None')
